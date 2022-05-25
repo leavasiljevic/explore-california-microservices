10e6023e0cb6ef9b1c8c4f66579a42431daea7fc
Lea <leavasiljevic@gmail.com>
2022-05-25 12:00:02 +0200

    Expose RESTfull APIs with Spring Data

--- src/main/java/calm/example/ec/repo/TourPackageRepository.java
+++ src/main/java/calm/example/ec/repo/TourPackageRepository.java
@@ -2,11 +2,47 @@ package calm.example.ec.repo;
 
 import calm.example.ec.domain.TourPackage;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.Optional;
 
+//renaming end-point
+@RepositoryRestResource(collectionResourceRel = "packages", path = "packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String> {
-    Optional<TourPackage> findByName(String name);
+
+    /**
+     * Find Tour Package by name.
+     *
+     * @param name name of the package
+     * @return Optional of TourPackage
+     */
+    Optional<TourPackage> findByName(@Param("name")String name);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> S save(S s);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends TourPackage> Iterable<S> saveAll(Iterable<S> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String s);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(TourPackage tourPackage);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends TourPackage> iterable);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
 }
 
 
